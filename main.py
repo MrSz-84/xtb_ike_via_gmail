bb75b1c63e3b2b51f8c02aa5aeddc4904e6b9af5
Marcin Borowski <pixelmotionhd@gmail.com>
2025-06-03 22:49:02 +0200

    main file using gmail api

--- main.py
+++ main.py
@@ -1,29 +1,72 @@
-from simplegmail import Gmail
-from simplegmail.query import construct_query
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 from config import consts as c
-import json
 
-with open('./config/docs.json', mode='r', encoding='utf-8') as creds:
-    docs = json.load(creds)
-gmail = Gmail(client_secret_file='./config/api_oath.json', creds_file='./config/gmail_token.json')
 
-params = {
-    'sender': c.SENDER,
-    'subject': c.SUBJECT,
-    'attachment': c.ATTACHMENT,
-    'spec_attachment': c.SPEC_ATTACHMENT,
-    'older_than': c.OLDER_THAN,
-    'newer_than': c.NEWER_THAN
-}
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
-messages = gmail.get_messages(query=construct_query(params))
-for message in messages:
-    print('To: ', message.recipient)
-    print('From: ', message.sender)
-    print('Subject: ', message.subject)
-    print('When: ', message.date)
-    print('Snippet: ', message.snippet)
-    print()
+def main():
     
-quit()
+    creds = None
     
+    if os.path.exists(c.TOKEN):
+        creds = Credentials.from_authorized_user_file(c.TOKEN, SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                c.SECRET, SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        with open(c.TOKEN, 'w') as token:
+            token.write(creds.to_json())
+    
+    query = f'from:{c.SENDER} subject:{c.SUBJECT} has:attachment filename:{c.SPEC_ATTACHMENT} older_than:1d newer_than:1m'
+                
+    try:
+        service = build(c.API_NAME, c.API_VERSION, credentials=creds)
+        results = service.users().messages().list(userId='me', maxResults=10, q=query).execute()
+        messages = results.get('messages', [])
+        
+        if not messages:
+            print('No messages found.')
+            return
+        print('Message:')
+        for message in messages:
+            msg = service.users().messages().get(userId='me', id=message['id'], format='full').execute()
+            
+            headers = msg['payload']['headers']
+            
+            sender = next((header['value'] for header in headers if header['name'] == 'From'), 'Brak nadawcy')
+            to = next((header['value'] for header in headers if header['name'] == 'To'), 'Brak odbiorcy')
+            subject = next((header['value'] for header in headers if header['name'] == 'Subject'), 'Brak tematu')
+            date = next((header['value'] for header in headers if header['name'] == 'Date'), 'Brak daty')
+            
+            attachments = 'Brak załączników'
+            if 'parts' in msg['payload']:
+                attachments = [part['filename'] for part in msg['payload']['parts']]
+                attachments = attachments if attachments else 'Brak załączników'
+            
+            snippet = msg.get('snippet', 'Brak podglądu')
+            print(f'ID: {msg['id']}')
+            print(f'Data: {date}')
+            print(f'Nadawca: {sender}')
+            print(f'Odbiorca: {to}')
+            print(f'Temat: {subject}')
+            print(f'Załączniki: {attachments}')
+            print(f'Podgląd: {snippet}')
+            print('-' * 80)
+
+            
+            
+    except HttpError as error:
+        print(f'An error occured: {error}')
+        
+if __name__ == "__main__":
+    main()
