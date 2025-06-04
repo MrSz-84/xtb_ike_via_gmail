b46f86f18e9b0ee5382ab3cb442ed3a8f4d54036
Marcin Borowski <pixelmotionhd@gmail.com>
2025-06-04 20:57:11 +0200

    aded const for read emails set file

--- config/consts.py
+++ config/consts.py
@@ -2,9 +2,12 @@ SENDER ='dailystatements@mail.xtb.com'
 SUBJECT = 'Potwierdzenie wykonania zleceń'
 ATTACHMENT = True
 SPEC_ATTACHMENT = 'pdf'
-OLDER_THAN = (1, 'day')
-NEWER_THAN = (1, 'month')
+OLDER_THAN = '0d' 
+#(1, 'day')
+NEWER_THAN = '1m'
+#(1, 'month')
 TOKEN = './config/token.json'
 SECRET = './config/api_oauth.json'
 API_NAME = 'gmail'
 API_VERSION = 'v1'
+READ_EMAILS = './files/already_read.txt'
