21e6bfb35b9083794ef8bf523ecf7598dd6eccf0
Marcin Borowski <pixelmotionhd@gmail.com>
2025-06-08 00:52:45 +0200

    added dataframe column names and data types

--- config/consts.py
+++ config/consts.py
@@ -9,3 +9,39 @@ SECRET = './config/api_oauth.json'
 API_NAME = 'gmail'
 API_VERSION = 'v1'
 READ_EMAILS = './files/already_read.txt'
+MERGED_DFS_NAMES = [
+    'Nr', 
+    'Numer zlecenia',
+    'Symbol',
+    'Nazwa instrumentu',
+    'System wykonania zlecenia',
+    'Typ', 
+    'Wolumen', 
+    'Data i czas transakcji',
+    'Zlecenie otwarcia',
+    'Cena jednostkowa',
+    'Cena całkowita',
+    'Klasa aktywów',
+    'Waluta kwotowania instrumentu',
+    'Kurs przewalutowania',
+    'Koszty przewalutowania',
+    'Prowizja',
+    'Łączne koszty']
+DATA_TYPES = {
+    'Nr': 'Int8', 
+    'Numer zlecenia': 'Int64',
+    'Symbol': 'string',
+    'Nazwa instrumentu': 'string',
+    'System wykonania zlecenia': 'string',
+    'Typ': 'string', 
+    'Wolumen': 'Float64', 
+    'Data i czas transakcji': 'datetime64[s]',
+    'Zlecenie otwarcia': 'string',
+    'Cena jednostkowa': 'Float64',
+    'Cena całkowita': 'Float64',
+    'Klasa aktywów': 'string',
+    'Waluta kwotowania instrumentu': 'string',
+    'Kurs przewalutowania': 'Float64',
+    'Koszty przewalutowania': 'Float64',
+    'Prowizja': 'Float64',
+    'Łączne koszty': 'Float64'}
