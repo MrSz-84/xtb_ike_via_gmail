f5576fbd9e8072790c99c9253df6e1cf356718aa
Marcin Borowski <pixelmotionhd@gmail.com>
2025-06-08 18:19:28 +0200

    Deleted names and data types for dropped columns Nr and Typ.

--- config/consts.py
+++ config/consts.py
@@ -10,12 +10,10 @@ API_NAME = 'gmail'
 API_VERSION = 'v1'
 READ_EMAILS = './files/already_read.txt'
 MERGED_DFS_NAMES = [
-    'Nr', 
     'Numer zlecenia',
     'Symbol',
     'Nazwa instrumentu',
     'System wykonania zlecenia',
-    'Typ', 
     'Wolumen', 
     'Data i czas transakcji',
     'Zlecenie otwarcia',
@@ -28,12 +26,10 @@ MERGED_DFS_NAMES = [
     'Prowizja',
     'Łączne koszty']
 DATA_TYPES = {
-    'Nr': 'Int8', 
     'Numer zlecenia': 'Int64',
     'Symbol': 'string',
     'Nazwa instrumentu': 'string',
     'System wykonania zlecenia': 'string',
-    'Typ': 'string', 
     'Wolumen': 'Float64', 
     'Data i czas transakcji': 'datetime64[s]',
     'Zlecenie otwarcia': 'string',
