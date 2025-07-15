76900eab869b77b5e9df51deec8dba7e63f7a704
Marcin Borowski <pixelmotionhd@gmail.com>
2025-07-15 22:27:58 +0200

    Async API call is finished. One exception was taken care of, when the user reaches API’s daily requests limit. More tests needed, because such calls have responses code 200… Also, simple argparse logic was implemented.

--- alpha_api_call_etfs.py
+++ alpha_api_call_etfs.py
@@ -1,4 +1,4 @@
-import requests, os, copy, datetime, argparse, json, time, asyncio
+import requests, os, copy, datetime, argparse, json, time, asyncio, aiohttp
 from google.cloud import storage
 from config import consts as c
 
@@ -6,11 +6,28 @@ with open('./config/xtb-ike-wallet-0a604e129e1a.json', mode='r', encoding='utf-8
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './config/xtb-ike-wallet-0a604e129e1a.json'
 with open(c.ALPHA_API, mode='r', encoding='utf-8') as f:
     os.environ['ALPHA_API_KEY'] = json.load(f).strip('"')
-os.environ['OUTPUT_SIZE_TYPE'] = c.ALPHA_OUTPUT_SIZE_TYPE
+# os.environ['OUTPUT_SIZE_TYPE'] = c.ALPHA_OUTPUT_SIZE_TYPE
 
-# TODO async version of the code - json parsing and ttl async corutine. Slower than synchroneous version. Tests needed for real API calls instead of reading from SSD.
-# TODO Make mechanism for creating scailable api call links
-# TODO build api calls function one for sync, and other for async for comparison.
+
+def argparse_logic():
+
+    parser = argparse.ArgumentParser(
+        prog='Alphavantage API Equity and FX data fetcher.',
+        description='''Not much to add hete. There are two options for data output size, compact and full. 
+        Try to use compact which returns 100 datapoints, because full returns 20+ years of daily data in some cases...''',
+        epilog='For more information wtite at pokeplacek@gmail.com.'
+    )
+    parser.add_argument(
+        '-os',
+        '--outputsize',
+        metavar='OS',
+        choices=['compact', 'full', 'test'],
+        default='compact',
+        type=str,
+        help='100 days vs 20+ years od data, choose compact :D'
+    )
+    args = parser.parse_args()
+    os.environ['OUTPUT_SIZE_TYPE'] = args.outputsize
 
 def get_symbols(responses):
     eq = 0
@@ -28,12 +45,12 @@ def get_symbols(responses):
             c.ALPHA_TICKER_SYMBOLS.append(symbol)
     c.ALPHA_EQ = eq
     c.ALPHA_FX = fx
-    
+
 def read_json(path):
     with open(path, mode='r', encoding='utf-8') as f:
         contents =  f.read()
         return json.loads(contents)
-    
+
 def parse_fx(api_res, min_max):
     from_symbol = api_res['Meta Data']['2. From Symbol']
     to_symbol = api_res['Meta Data']['3. To Symbol']
@@ -41,12 +58,12 @@ def parse_fx(api_res, min_max):
     values = 'Time Series FX (Daily)'
     meta = {'from': from_symbol, 'to': to_symbol, 'symbol': pair}
     return _parse_frame(api_res[values], meta, min_max)
-    
+
 def parse_equity(api_res, min_max):
     symbol = api_res['Meta Data']['2. Symbol'].replace('.LON', '.UK')
     values = 'Time Series (Daily)'
     return _parse_frame(api_res[values], {'symbol': symbol}, min_max)
-    
+
 def _parse_frame(time_series, metadata, min_max):
     dct = {}
     for k, v in time_series.items():
@@ -57,18 +74,18 @@ def _parse_frame(time_series, metadata, min_max):
         else:
             dct[k] = tmp_dct
     return dct
-    
+
 def parse_all_api_res(api_res, data_type, min_max):
     if data_type == 'fx':
         return parse_fx(api_res, min_max)
     else:
         return parse_equity(api_res, min_max)
-        
+
 def files_cleanup(paths):
     for path in paths:
         if os.path.exists(path):
             os.remove(path)
-    
+
 def create_csv(batch: list[dict[dict]]):
     new_min_max = {}
     files_cleanup((c.ALPHA_EQUITY_CSV, c.ALPHA_FX_CSV))
@@ -92,7 +109,7 @@ def create_csv(batch: list[dict[dict]]):
                     f.write(line)
             new_min_max[list(equity.values())[0]['symbol']] = {'min_date': min(equity.keys()), 'max_date': max(equity.keys())}
     return new_min_max
-    
+
 def read_min_max(path):
     with open(path, mode='r', encoding='utf-8') as f:
         header = f.readline().strip().split(',')
@@ -101,7 +118,7 @@ def read_min_max(path):
             for line in (l.strip().split(',') for l in f)
             }
     return min_max
-    
+
 def min_max_compare(old_min_max, new_min_max):
     swapped_min_max = copy.deepcopy(old_min_max)
     symbols = [s for s in old_min_max]
@@ -111,7 +128,7 @@ def min_max_compare(old_min_max, new_min_max):
         if new_min_max[symbol]['max_date'] > old_min_max[symbol]['max_date']:
             swapped_min_max[symbol]['max_date'] = new_min_max[symbol]['max_date']
     return write_min_max(swapped_min_max)
-    
+
 def write_min_max(data):
     header = 'symbol,min_date,max_date\n'
     with open(c.ALPHA_MIN_MAX, mode='w', encoding='utf-8') as f:
@@ -119,7 +136,7 @@ def write_min_max(data):
         for symbol, dates in data.items():
             line = f'{symbol},' + ','.join(date for date in dates.values()) + '\n'
             f.write(line)
-    
+
 def is_parsed_empty(parsed):
     is_empty = []
     for elem in parsed:
@@ -128,7 +145,7 @@ def is_parsed_empty(parsed):
         else:
             is_empty.append(False)
     return any(is_empty)
-    
+
 def create_boilerplate_min_max_file(symbols):
     dates = {'min_date': '2025-01-01', 'max_date': '2025-01-31'}
     header = 'symbol,min_date,max_date\n'
@@ -137,7 +154,7 @@ def create_boilerplate_min_max_file(symbols):
         for symbol in symbols:
             line = f'{symbol},{dates['min_date']},{dates['max_date']}\n'
             f.write(line)
-    
+
 def upload_to_bucket(fname, gsbucket, dest_fname):
     dest_fname = dest_fname.replace('./tmp/','')
     client = storage.Client()
@@ -145,7 +162,7 @@ def upload_to_bucket(fname, gsbucket, dest_fname):
     blob = bucket.blob(dest_fname)
     blob.upload_from_filename(fname)
     print(f'⬆️ Upload of the file {fname} to {gsbucket} cloud storage bucket complete.')
-    
+
 def download_from_bucket(source_fname, gsbucket, dest_fname):
     source_fname = dest_fname.replace('./tmp/','')
     client = storage.Client()
@@ -157,70 +174,6 @@ def download_from_bucket(source_fname, gsbucket, dest_fname):
     else:
         print(f'ℹ️ Cloud Storage file object not found. Creating empty file...  {dest_fname}')
         create_boilerplate_min_max_file(c.ALPHA_TICKER_SYMBOLS)
-
-
-# def validate_dates(dates):
-#     start = datetime.datetime.strptime(dates[0], '%Y-%m-%d')
-#     stop = datetime.datetime.strptime(dates[1], '%Y-%m-%d')
-#     if start <= stop:
-#         return dates
-#     else:
-#         return [stop.strftime('%Y-%m-%d'), start.strftime('%Y-%m-%d')]
-
-# def argparse_logic():
-#     start = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-#     stop = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-
-#     parser = argparse.ArgumentParser(
-#         prog='NBP USD_PLN currency exchange rate fetcher',
-#         description='''Fetch currency exchange rates data from NBP\'s API for for last day,
-#         or given date period (up to 93 days). Use flags -l for last day or -p for givben period.
-#         Type -h for help''',
-#         epilog='For more information wtite at pokeplacek@gmail.com.'
-#     )
-#     group = parser.add_mutually_exclusive_group()
-#     group.add_argument(
-#         '-l',
-#         '--last',
-#         action='store_true',
-#         help='Fetch currency exchange rates for previous day.'
-#     )
-#     group.add_argument(
-#         '-p',
-#         '--period',
-#         action='store_true',
-#         help='Fetch currency exchange rates for given period up to 93 days'
-#     )
-#     parser.add_argument(
-#         '-d',
-#         '--dates',
-#         metavar='D',
-#         nargs=2,
-#         default=[start, stop],
-#         type=str,
-#         help='Dates in ISO format for --period option. Max period length 93 days.'
-#     )
-#     args = parser.parse_args()
-#     if not args.last and not args.period:
-#         args.last = True
-#     args.dates = validate_dates(args.dates)
-#     if args.last:
-#         os.environ['NBP_REQ_TYPE'] = 'last'
-#     else:
-#         os.environ['NBP_REQ_TYPE'] = 'period'
-#     os.environ['NBP_START_DATE'] = args.dates[0]
-#     os.environ['NBP_END_DATE'] = args.dates[1]
-
-def get_data(url):
-    r = requests.get(url)
-    if r.status_code == 200:
-        return r.json()
-    elif r.status_code == 404:
-        print(f'API call for mid raiting returned {r.status_code}.')
-        exit()
-    elif r.status_code == 400:
-        print(f'API call for mid raiting returned {r.status_code}.')
-        exit()
 
 def create_fx_url(pair, base_req):
     from_s = pair[:3]
@@ -245,29 +198,61 @@ def create_requests_lst(base_req, symbols):
             requests_lst.append(create_eq_url(symbol, base_req[stype]))
     return requests_lst
 
+async def get_data_async(sess, url):
+    async with sess.get(url) as r:
+        rtext = await r.text()
+        limit = 'to instantly remove all daily rate limits'
+        if r.status == 200 and limit not in rtext:
+            return await r.json()
+        elif r.status == 200 and limit in rtext:
+            raise Exception(f'Max daily limit of API calls reached.')
+        elif r.status == 400:
+            print(f'API call for mid raiting returned {r.status_code}.')
+            exit()
 
+def upload_min_max():
+    try:
+        upload_to_bucket(c.ALPHA_MIN_MAX, c.ALPHA_MIN_MAX_BUCKET, c.ALPHA_MIN_MAX)
+    except Exception as e:
+        files_cleanup([c.ALPHA_EQUITY_CSV, c.ALPHA_FX_CSV, c.ALPHA_MIN_MAX])
+        print(f'❌ An error occured during upload file {c.ALPHA_MIN_MAX} to bucket: {e}')
 
-def main():
+def upload_equity():
+    try:
+        upload_to_bucket(c.ALPHA_EQUITY_CSV, c.ALPHA_EQ_BUCKET, c.ALPHA_EQUITY_CSV)
+    except Exception as e:
+        files_cleanup([c.ALPHA_EQUITY_CSV, c.ALPHA_FX_CSV, c.ALPHA_MIN_MAX])
+        print(f'❌ An error occured during upload file {c.ALPHA_EQUITY_CSV} to bucket: {e}')
+        upload_min_max()
+
+def upload_fx():
+    try:
+        upload_to_bucket(c.ALPHA_FX_CSV, c.ALPHA_FX_BUCKET, c.ALPHA_FX_CSV)
+    except Exception as e:
+        files_cleanup([c.ALPHA_EQUITY_CSV, c.ALPHA_FX_CSV, c.ALPHA_MIN_MAX])
+        print(f'❌ An error occured during upload file {c.ALPHA_FX_CSV} to bucket: {e}')
+        upload_min_max()
+
+async def main():
     start = time.time()
+    argparse_logic()
     requests_lst = create_requests_lst(c.ALPHA_REQ_TYPE, c.ALPHA_REQ_SYMBOLS)
-    tasks = []
-    for req in requests_lst:
-        tasks.append(get_data(req))
-    # for task in tasks:
-    #     print(task['Meta Data'])
-    # print(len(tasks))
+    async with aiohttp.ClientSession() as session:
+        tasks = [get_data_async(session, req) for req in requests_lst]
+        try:
+            responses = await asyncio.gather(*tasks)
+        except Exception as e:
+            print(f'❌ An error occured during API call: {e}')
+            return
     # files = ['./tmp/eimi_api.json', './tmp/igln_api.json', './tmp/iwda_api.json', './tmp/fx_usdpln_api.json', './tmp/fx_eurpln_api.json']
     # tasks = [read_json(file) for file in files]
-    get_symbols(tasks)
+    get_symbols(responses)
     # TODO Uncomment download from bucket, and delete create_boilerplate...()
-    download_from_bucket(c.ALPHA_MIN_MAX, c.MAIL_IDS_PATH, c.ALPHA_MIN_MAX)
-    create_boilerplate_min_max_file(c.ALPHA_TICKER_SYMBOLS)
-    # results = tasks
+    download_from_bucket(c.ALPHA_MIN_MAX, c.ALPHA_MIN_MAX_BUCKET, c.ALPHA_MIN_MAX)
+    # create_boilerplate_min_max_file(c.ALPHA_TICKER_SYMBOLS)
     
     min_max = read_min_max(c.ALPHA_MIN_MAX)
-    
-    # for i, res in enumerate(tasks):
-    parsed = [parse_all_api_res(res, data_type='etf' if i < c.ALPHA_EQ else 'fx', min_max=min_max) for i, res in enumerate(tasks)]
+    parsed = [parse_all_api_res(res, data_type='etf' if i < c.ALPHA_EQ else 'fx', min_max=min_max) for i, res in enumerate(responses)]
     if is_parsed_empty(parsed):
         print(f'ℹ️ No new data was found after parsing API calls. Cleaning up and shutting down...')
         files_cleanup([c.ALPHA_EQUITY_CSV, c.ALPHA_FX_CSV, c.ALPHA_MIN_MAX])
@@ -277,34 +262,13 @@ def main():
     old_min_max = read_min_max(c.ALPHA_MIN_MAX)
     min_max_compare(old_min_max, new_min_max)
     
-    # argparse_logic()
-    # req_pair = create_requests_url(base_req=c.NBP_BASE_REQ, type=os.environ['NBP_REQ_TYPE'])
-    # input_ = get_data(req_pair)
-    # if input_[0]['code'] == 'empty' and input_[1]['code'] == 'empty':
-    #     print(f'❌ Mid table response status: {input_[0]['rates']}')
-    #     print(f'❌ Ask|Bid table response status: {input_[1]['rates']}')
-    #     return
-    # output = create_data_struct(input_)
-    # create_csv(output)
-    
-    try:
-        upload_to_bucket(c.ALPHA_EQUITY_CSV, c.ALPHA_EQ_BUCKET, c.ALPHA_EQUITY_CSV)
-    except Exception as e:
-        files_cleanup([c.ALPHA_EQUITY_CSV, c.ALPHA_FX_CSV, c.ALPHA_MIN_MAX])
-        print(f'❌ An error occured during upload file {c.ALPHA_EQUITY_CSV} to bucket: {e}')
-        return
-    try:
-        upload_to_bucket(c.ALPHA_FX_CSV, c.ALPHA_FX_BUCKET, c.ALPHA_FX_CSV)
-    except Exception as e:
-        files_cleanup([c.ALPHA_EQUITY_CSV, c.ALPHA_FX_CSV, c.ALPHA_MIN_MAX])
-        print(f'❌ An error occured during upload file {c.ALPHA_FX_CSV} to bucket: {e}')
-        return
-    try:
-        upload_to_bucket(c.ALPHA_MIN_MAX, c.ALPHA_MIN_MAX_BUCKET, c.ALPHA_MIN_MAX)
-    except Exception as e:
-        files_cleanup([c.ALPHA_EQUITY_CSV, c.ALPHA_FX_CSV, c.ALPHA_MIN_MAX])
-        print(f'❌ An error occured during upload file {c.ALPHA_MIN_MAX} to bucket: {e}')
-        return
+    if 'equity' in c.ALPHA_REQ_SYMBOLS.values():
+        upload_equity()
+        print('in equity')
+    if 'fx' in c.ALPHA_REQ_SYMBOLS.values():
+        upload_fx()
+        print('in fx')
+    upload_min_max()
     
     stop = time.time()
     print('Duration: ', stop - start)
@@ -313,4 +277,4 @@ def main():
     print('✅ All tasks done, finishing program...') 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
